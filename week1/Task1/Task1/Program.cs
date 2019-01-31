a8f4e0e1133bd3b1f347b40a7073648575c2dc9b
Nurmaganbet <smatov_00@mail.ru>
2019-01-31 16:24:10 +0600

    Lab1

--- week1/Task1/Task1/Program.cs
+++ week1/Task1/Task1/Program.cs
@@ -8,8 +8,42 @@ namespace Task1
 {
     class Program
     {
-        static void Main(string[] args)
-        {
+                public static bool Isprime(int a) //Function to determine the prime number
+                {
+                if (a == 1)
+                    return false;
+                int cnt = 0;
+                for (int i = 2; i <= a; i++)
+                {
+                    if (a % i == 0) cnt++;
+                }
+                if (cnt == 1) return true;
+                return false;
+                }
+
+                static void Main(string[] args)
+                {
+                int n = int.Parse(Console.ReadLine()); //I set the size of the array and declare the array 
+                int[] a = new int[n];
+                int x = n;
+                string[] arr = Console.ReadLine().Split(); //I write the values ​​of the type string into the array
+                for (int i = 0; i < n; i++) //change string to integer 
+                {
+                    a[i] = int.Parse(arr[i]);
+                }
+                for(int i = 0; i < n; i++)
+                {
+                if (!Isprime(a[i]))
+                    x--;    
+                }
+                Console.WriteLine(x);
+                for (int i = 0; i < a.Length; i++)//checking all numbers in array with function isPrime
+                {
+                    if (Isprime(a[i]))
+                    Console.Write(a[i] + " ");
+                }
+                Console.ReadKey();
+            }
         }
     }
-}
+

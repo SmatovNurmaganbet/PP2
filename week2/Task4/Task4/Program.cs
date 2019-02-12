75fe3d83802ed205bfae2d72e60c1306c562bddb
Nurmaganbet <smatov_00@mail.ru>
2019-02-12 16:47:47 +0600

    Lab2

--- week2/Task4/Task4/Program.cs
+++ week2/Task4/Task4/Program.cs
@@ -11,18 +11,18 @@ namespace Task4
     {
         static void Main(string[] args)
         {
-            string path1 = "C:/Users/Aidos/Documents/PP2/week2/Task4/path1/t.txt";
-            string path2 = "C:/Users/Aidos/Documents/PP2/week2/Task4/path2/t.txt";
+            string path1 = "C:/Users/Aidos/Documents/PP2/week2/Task4/path1/t.txt";//path1 of txt file
+            string path2 = "C:/Users/Aidos/Documents/PP2/week2/Task4/path2/t.txt";//path2 of txt file
 
-            StreamWriter sw = new StreamWriter(path1);
-            sw.Write("asd");
-            sw.Close();
-            FileInfo f = new FileInfo(path1);
+            StreamWriter sw = new StreamWriter(path1);//create a streamwriter 
+            sw.Write("asd");//we create txt file 
+            sw.Close();//close the streamwriter
+            FileInfo f = new FileInfo(path1);//create a new fileinfo and send the meaning of path1  
 
-            if (f.Exists)
+            if (f.Exists)//if file f exists then do following things
             {
-                f.CopyTo(path2, true);
-                f.Delete();
+                f.CopyTo(path2, true);//we copy txt file from path1 to path2
+                f.Delete();//delete the txt file in path1
             }
         }
     }
